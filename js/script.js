e129812c8573db12a169ebdf8e623f2a71dc03af
MarcoBagnasco <bagna.hbk@gmail.com>
2021-03-27 11:03:26 +0100

    add click event

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('hello');
+/* 
+CREATE YOUR BURGER
+*/
+
+// REFERENCE
+var btn = document.getElementById('button');
+console.log(btn); //test
+
+// Calculate Price
+btn.addEventListener('click', function(){
+    console.log('click'); //test
+})
