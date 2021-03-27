ad0d7272250b5058c9f5d30baa9849d561c97ffb
MarcoBagnasco <bagna.hbk@gmail.com>
2021-03-27 11:12:44 +0100

    add burger-name required

--- js/script.js
+++ js/script.js
@@ -5,8 +5,18 @@ CREATE YOUR BURGER
 // REFERENCE
 var btn = document.getElementById('button');
 console.log(btn); //test
+var burgerName = document.getElementById('name');
+console.log(burgerName); //test
 
 // Calculate Price
 btn.addEventListener('click', function(){
-    console.log('click'); //test
+    // console.log('click'); //test
+    var name = burgerName.value.trim();
+    console.log(name); //test
+    // BurgerName required
+    if(name.length === 0){
+        alert('Insert Burger Name');
+    } else{
+        console.log('proceed with the calculation ');
+    }
 })
